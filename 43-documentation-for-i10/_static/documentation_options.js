6e3b361f24b582eab9197ad396e9c6db771f3487
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2025-03-24 11:49:45 +0000

    deploy: b5a0b56d2d707320dc1f219cd38bd4eb6c81aa0a

--- 43-documentation-for-i10/_static/documentation_options.js
+++ 43-documentation-for-i10/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.5.dev62+g5c9f2cb',
+    VERSION: '0.0.0.5.dev63+gb5a0b56',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
